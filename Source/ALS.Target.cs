c5744bf4fc3b20f4cb7f5ede03d6057dd18e9e45
wantg <wantg78@gmail.com>
2024-09-08 19:23:33 +0800

    Update to 5.4

--- Source/ALS.Target.cs
+++ Source/ALS.Target.cs
@@ -8,8 +8,8 @@ public class ALSTarget : TargetRules
 	public ALSTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.AddRange( new string[] { "ALS" } );
 	}
 }
--- Source/ALSEditor.Target.cs
+++ Source/ALSEditor.Target.cs
@@ -8,7 +8,7 @@ public class ALSEditorTarget : TargetRules
 	public ALSEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.AddRange( new string[] { "ALS" } );
 	}
